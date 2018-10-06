1be3c139d5dc3243eb129d35e72f02a828136c24
16CO142 <shreyaspandith1210@gmail.com>
2018-10-06 22:01:04 +0530

    updated navbar

--- article/public/js/gnmenu.js
+++ article/public/js/gnmenu.js
@@ -49,6 +49,12 @@
 					self._openMenu(); 
 					document.addEventListener( self.eventtype, self.bodyClickFn ); 
 				} );
+
+				this.menu.addEventListener( 'mouseout', function(ev) {
+					self._closeMenu(); 
+					document.removeEventListener( self.eventtype, self.bodyClickFn ); 
+				} );
+
 			}
 			this.trigger.addEventListener( this.eventtype, function( ev ) {
 				ev.stopPropagation();
